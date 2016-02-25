c41eca0af328075c27e2f5e9c14d36703c80b8c4
Akhil Subbarao <akhilsubbarao98@gmail.com>
2016-02-25 01:10:47 -0800

    Not much.

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -7,14 +7,49 @@ public class Dijkstra implements Shorty {
 	String startNode;
 	private ArrayList<Boolean> k; // Selected set
 	private ArrayList<String> nodeNames; // node names
-	private ArrayList<String> previousNode; // previous node in the shortest path
-	private ArrayList<Integer> startNodeDistance; // distance to this node from, the start node
+	private ArrayList<String> previousNode; // previous node in the shortest
+											// path
+	private ArrayList<Double> nodeDistance; // distance to this node from,
+											// the start node
 	public ArrayList<String> nodeIndices; // used primarily to retrieve the
 											// indices of various nodes
-	public String anchorNode = "A";
-	
+
 	public Dijkstra(HashMap<String, HashMap<String, Integer>> nodeDistances,
 			String start) {
+		// setting all 6 nodes to false in the k arraylist, as none are terminal
+		// nodes yet
+		k.set(0, false);
+		k.set(1, false);
+		k.set(2, false);
+		k.set(3, false);
+		k.set(4, false);
+		k.set(5, false);
+
+		// adding all the nodes in the nodeNames arrayList
+		nodeNames.set(0, "A");
+		nodeNames.set(1, "B");
+		nodeNames.set(2, "C");
+		nodeNames.set(3, "D");
+		nodeNames.set(4, "E");
+		nodeNames.set(5, "F");
+
+		// All previous nodes are set to NA (Not Availabe) because there is no
+		// starting yet, therefore none have a previous node
+		previousNode.set(0, "NA");
+		previousNode.set(1, "NA");
+		previousNode.set(2, "NA");
+		previousNode.set(3, "NA");
+		previousNode.set(4, "NA");
+		previousNode.set(5, "NA");
+
+		// All distances are inifity because they have not been reached yet, and
+		// therefore have no distance yet
+		nodeDistance.set(0, Double.POSITIVE_INFINITY);
+		nodeDistance.set(1, Double.POSITIVE_INFINITY);
+		nodeDistance.set(2, Double.POSITIVE_INFINITY);
+		nodeDistance.set(3, Double.POSITIVE_INFINITY);
+		nodeDistance.set(4, Double.POSITIVE_INFINITY);
+		nodeDistance.set(5, Double.POSITIVE_INFINITY);
 
 		/**
 		 * I want to fill those three data structures in the slides. I know I am
@@ -24,10 +59,9 @@ public class Dijkstra implements Shorty {
 		 */
 		// nodes and prev are equivalent to path as follows
 		/**
-		 * take one node and enter value into nodename, prevnode, start nodedistance
-		 * while loop to continously fill arraylists up and update information
-		 * 
+		 * take one node and enter value into nodename, prevnode, start
+		 * nodedistance while loop to continously fill arraylists up and update
+		 * information
 		 */
 	}
-
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Set;
 
 public class Graph {
 	HashMap<String, HashMap<String, Integer>> nodesOnGraph = new HashMap<String, HashMap<String, Integer>>();
@@ -13,17 +14,17 @@ public class Graph {
 		HashMap<String, Integer> nodeB = new HashMap<String, Integer>();
 		HashMap<String, Integer> nodeC = new HashMap<String, Integer>();
 		HashMap<String, Integer> nodeD = new HashMap<String, Integer>();
+		HashMap<String, Integer> nodeE = new HashMap<String, Integer>();
 		HashMap<String, Integer> nodeF = new HashMap<String, Integer>();
-		HashMap<String, Integer> nodeG = new HashMap<String, Integer>();
 
 		nodeA.put("A", 0);
 		nodeA.put("C", 2);
-		nodeA.put("G", 3);
-		nodeA.put("F", 7);
+		nodeA.put("F", 3);
+		nodeA.put("E", 7);
 
 		nodeB.put("B", 0);
-		nodeB.put("G", 6);
-		nodeB.put("F", 7);
+		nodeB.put("F", 6);
+		nodeB.put("E", 7);
 		nodeB.put("D", 9);
 
 		nodeC.put("C", 0);
@@ -31,25 +32,25 @@ public class Graph {
 		nodeC.put("D", 13);
 
 		nodeD.put("D", 0);
-		nodeD.put("F", 2);
+		nodeD.put("E", 2);
 		nodeD.put("C", 2);
 
-		nodeF.put("F", 0);
-		nodeF.put("A", 3);
-		nodeF.put("C", 15);
-		nodeF.put("D", 31);
+		nodeE.put("E", 0);
+		nodeE.put("A", 3);
+		nodeE.put("C", 15);
+		nodeE.put("D", 31);
 
-		nodeG.put("G", 0);
-		nodeG.put("A", 1);
-		nodeG.put("B", 3);
-		nodeG.put("F", 6);
+		nodeF.put("F", 0);
+		nodeF.put("A", 1);
+		nodeF.put("B", 3);
+		nodeF.put("E", 6);
 
 		nodesOnGraph.put("A", nodeA);
 		nodesOnGraph.put("B", nodeB);
 		nodesOnGraph.put("C", nodeC);
 		nodesOnGraph.put("D", nodeD);
+		nodesOnGraph.put("E", nodeE);
 		nodesOnGraph.put("F", nodeF);
-		nodesOnGraph.put("G", nodeG);
 
 		getGraph();
 	}
@@ -57,6 +58,8 @@ public class Graph {
 	public HashMap<String, HashMap<String, Integer>> getGraph() {
 		System.out.println("Here are the connections for node A: "
 				+ nodesOnGraph.get("A"));
+		Set keys = nodesOnGraph.keySet();
+		System.out.println(keys);
 		return nodesOnGraph;
 	}
 }
